c2c265de87c9ea31a009ae80f04f20d8d293c4d9
nazishzaman <168236857+nazishzaman@users.noreply.github.com>
2024-11-05 06:38:44 -0800

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nevbar = () => {
+  return (
+    <div className='bg-slate-400 w-11 h-[100px]'>
+    <link href='/'>Home</link>
+    <link href='/'>about</link>
+    <link href='/'>contact</link>
+
+      
+    </div>
+  )
+} 
+
+export default Nevbar;
+
